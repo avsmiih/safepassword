f4e7d5dba19bf78025e14c1e5f1624082be374ea
Miriã Tavares Machado <71910381+avsmiih@users.noreply.github.com>
2023-09-25 16:02:08 -0300

    JWT - atualizações

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sistema de Login</title>
+</head>
+<body>
+    <div>
+        <img src="https://www.ma.gov.br/assets/images/brand/gov__ma.png">
+        
+        <form action="result.php" method="post">
+            <div>
+                <h2> login </h2>
+            </div>
+            <div>
+                <div>
+                    <label for="usuario">Usuário:</label>
+                    
+                    <input type="text" name="usuario" autocomplete="off" required='true'>
+
+                </div>
+                <div>
+                    <label for="password">Senha:</label>
+
+                    <input type="password" name="senha" autocomplete="off" required='true'>
+
+                </div>                
+            </div>
+            <div>
+                <button type="submit" value="login" class="submit"> enviar</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
