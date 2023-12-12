f421eee6b8c3d3daa3e35896834498b923b5aa2e
Miriã Tavares Machado <71910381+avsmiih@users.noreply.github.com>
2023-12-12 19:33:05 -0300

    tela de erro

---
+++ telas_sistema/tela_erro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="shortcut icon" type="imagex/png" href="../imagens/sistema-seguro.ico">
+    <title>Acesso negado</title>
+</head>
+<body>
+<div>
+    <div>
+        <img id="error" src="https://www.ma.gov.br/assets/images/brand/gov__ma.png">
+        <img id="error" src="../imagens/pngwing.com.png">
+    </div>
+    
+    <div>
+    <h4>
+        Algo está errado!
+        Por favor, retorne a página de acesso! <h4>
+    </div>
+    
+    <div id="button-error">
+    <?php include("botao_inicial.html") ?>
+    </div> 
+</div>
+</body>
+</html>
