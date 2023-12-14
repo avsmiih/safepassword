af7f9510e4db6654ce0cf538f6a3de219934b6c2
Miriã Tavares Machado <71910381+avsmiih@users.noreply.github.com>
2023-12-14 12:01:07 -0300

    Add files via upload

--- result.php
+++ result.php
@@ -8,37 +8,35 @@ if(!isset($_SESSION)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
     <link rel="shortcut icon" type="imagex/png" href="imagens/sistema-seguro.ico">
+    <link rel="stylesheet" href="result.css">
     <title>Credenciamento - Sistema Seguro</title>
 </head>
 <body>
-    <div id="login">
-    <img class=result src="https://www.ma.gov.br/assets/images/brand/gov__ma.png">
-
-        <div>
-            <p>
-                <?php
-                include('token.php');
-
-                echo '
-                    <h4> Login realizado com sucesso! Seja bem vindo '. 
-                    $_SESSION['user']. '<h4>' .
-                    '<h5> Este é o seu token, guarde-o com segurança!
-                    Ele será requisitado a você em nosso sistema. <h5>' .
-                    '<h5> Token de acesso: ' . $token . '<h5>';
-
-                $_SESSION['auth_user'] = $token;
-
-                include('atualizaçao.php');
-                echo '<br><br>';
-                include('botao2.html');
-                include('botao4.html');
-
-                ?>
-            </p>
+    <main>
+        <div id="box-imagem">
+            <img src="imagens/governo.jpeg" alt="imagem">
         </div>
-    </div>
+
+        <h2>Login realizado com sucesso! Seja bem vindo(a) <?php echo $_SESSION['user']; ?></h2>
+        <br>
+
+        <p id="paragrafo">Este é o seu token, guarde-o com segurança! Ele será requisitado a você em nosso sistema.</p>
+
+        <div id="box-token">
+            <h2>Token de acesso:</h2>
+            <p id="token"><?php include ('token.php'); echo $token ?></p>
+        </div>
+            <button>copy</button>
+        
+
+        <?php
+        $_SESSION['auth_user'] = $token;
+        include('atualizaçao.php');
+        include('botao2.html');
+        include('botao4.html');
+        ?>
+    </main>
 </body>
 
 </html>
